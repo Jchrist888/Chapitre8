e0472ab9beb85658be7d74c8a407a0196b55de5a
I1-P151 <I1-P151@Pinfo1.local>
2026-06-04 09:27:19 +0000

    Ajout des tests BookShelf et correction de JUnit

--- src/bookstoread/BookShelf.java
+++ src/bookstoread/BookShelf.java
@@ -1,19 +1,32 @@
 package bookstoread;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class BookShelf {
     private final List<String> books = new ArrayList<>();
 
-    // Retourne la liste des livres
     public List<String> books() {
-        return books;
+        //return books;
+
+        return Collections.unmodifiableList(books);
     }
 
-    // Ajoute plusieurs livres à la fois
+   /* public void add(String bookToAdd) {
+        books.add(bookToAdd);
+    }
+
+    */
+
     public void add(String... booksToAdd) {
+
         books.addAll(Arrays.asList(booksToAdd));
+    }
+
+
+    public List<String> arrange() {
+        books.sort(Comparator.naturalOrder());
+        return books;
+
+
     }
 }
